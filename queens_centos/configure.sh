24619d85b7b2c21bd5918f041417e4f25c6803da
Jaehyun Nam <namjh@kaist.ac.kr>
2018-06-09 19:41:53 +0900

    minor updates

--- queens_centos/configure.sh
+++ queens_centos/configure.sh
@@ -9,6 +9,3 @@ wget http://www.sdx4u.net/downloads/xenial-server-cloudimg-amd64-disk1.img
 
 openstack image create "Ubuntu14.04" --file trusty-server-cloudimg-amd64-disk1.img --disk-format qcow2 --container-format bare --public
 openstack image create "Ubuntu16.04" --file xenial-server-cloudimg-amd64-disk1.img --disk-format qcow2 --container-format bare --public
-
-cp httpd/conf.d/15-horizon_vhost.conf /etc/httpd/conf.d/15-horizon_vhost.conf
-cp neutron/dhcp_agent.ini /etc/neutron/dhcp_agent.ini
