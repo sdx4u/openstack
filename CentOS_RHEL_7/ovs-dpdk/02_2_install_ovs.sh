09d4af97062add91a8826ab2cbc204b12595f282
Jaehyun Nam <namjh@kaist.ac.kr>
2018-12-29 01:13:32 +0900

    minor updates

--- CentOS_RHEL_7/ovs-dpdk/02_2_install_ovs.sh
+++ CentOS_RHEL_7/ovs-dpdk/02_2_install_ovs.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
 
-if [ "$1" = "2.6" ];
-then
-	mkdir ~/build
-	cp ovs/* ~/build && cd ~/build
+sudo yum install -y libibverbs
 
-	yum install -y libcap libcap-devel libcap-ng-devel
-	#yum install -y openvswitch-2.6.3-1.el7.x86_64.rpm
+sudo systemctl start openvswitch
+sudo systemctl enable openvswitch
 
-	systemctl start openvswitch
-	systemctl enable openvswitch
+sleep 5
 
-	sleep 5
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
+sudo systemctl restart openvswitch
 
-	ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
-	systemctl restart openvswitch
-
-	echo "Please reboot the machine"
-elif [ "$1" = "2.8" ];
-then
-        mkdir ~/build
-        cp ovs/* ~/build && cd ~/build
-
-        yum install -y libcap libcap-devel libcap-ng-devel
-        #yum install -y openvswitch-2.8.4-1.el7.centos.x86_64.rpm
-
-        systemctl start openvswitch
-        systemctl enable openvswitch
-
-        sleep 5
-
-        ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
-        systemctl restart openvswitch
-
-        echo "Please reboot the machine"
-else
-	echo "$0 [ 2.6 | 2.8 ]"
-fi
+echo "Please reboot the machine"
