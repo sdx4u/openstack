c8eb9e3705bc46511faccba1fcc533287a0ac038
Jaehyun Nam <namjh@kaist.ac.kr>
2019-01-12 00:43:56 +0900

    minor updates

--- CentOS_RHEL_7/ovs-dpdk/bin/boot-dpdk.sh
+++ CentOS_RHEL_7/ovs-dpdk/bin/boot-dpdk.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+EX_INT=enp216s0f0
+VL_INT=enp59s0f0
+
 modprobe vfio-pci
 /usr/bin/chmod a+x /dev/vfio
 /usr/bin/chmod 0666 /dev/vfio/*
 
-#dpdk-devbind --bind=vfio-pci enp59s0f0
+# br-ex
+#dpdk-devbind --bind=vfio-pci $EX_INT
+
+# br-vlan
+#dpdk-devbind --bind=vfio-pci $VL_INT
--- CentOS_RHEL_7/ovs-dpdk/bin/kill-dpdk.sh
+++ CentOS_RHEL_7/ovs-dpdk/bin/kill-dpdk.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-#dpdk-devbind --bind=i40e 0000:3b:00.0
+EX_DEV=0000:d8:00.0
+VL_DEV=0000:3b:00.0
+
+# br-ex
+#dpdk-devbind --bind=ixgbe $EX_DEV
+
+# br-vlan
+#dpdk-devbind --bind=i40e $VL_DEV
