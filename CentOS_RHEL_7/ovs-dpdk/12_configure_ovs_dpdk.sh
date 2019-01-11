76fa7822f1a04e15eca4aee79751074234a03a6c
Jaehyun Nam <namjh@kaist.ac.kr>
2019-01-12 00:48:14 +0900

    minor updates

--- CentOS_RHEL_7/ovs-dpdk/12_configure_ovs_dpdk.sh
+++ CentOS_RHEL_7/ovs-dpdk/12_configure_ovs_dpdk.sh
@@ -3,7 +3,9 @@
 EX_DEV=0000:d8:00.0
 VL_DEV=0000:3b:00.0
 
-ovs-vsctl add-port br-ex dpdk0 -- set Interface dpdk0 type=dpdk options:dpdk-devargs=$EX_DEV
-ovs-vsctl add-port br-vlan dpdk1 -- set Interface dpdk1 type=dpdk options:dpdk-devargs=$VL_DEV
+ovs-vsctl add-port br-ex dpdk0 -- set Interface dpdk0 type=dpdk \
+          options:dpdk-devargs=$EX_DEV
+ovs-vsctl add-port br-vlan dpdk1 -- set Interface dpdk1 type=dpdk \
+          options:dpdk-devargs=$VL_DEV
 
 ovs-vsctl show
